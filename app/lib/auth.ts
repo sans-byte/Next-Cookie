bf98e3f26402109c2ab775fb7f5bcf934cdc6cb8
sanskar jain <sanskarj15@gmail.com>
2024-08-23 18:30:57 +0530

    added google auth

--- app/lib/auth.ts
+++ app/lib/auth.ts
@@ -1,7 +1,12 @@
 import CredentialsProvider from "next-auth/providers/credentials";
+import GoogleProvider from "next-auth/providers/google";
 
 export const NEXT_AUTH = {
   providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID || "",
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
+    }),
     CredentialsProvider({
       name: "Email",
       credentials: {
